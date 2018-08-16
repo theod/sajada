754273767d4f5e3cb4df5d3d1d5a8e13675aaa85
theod <theo.delahogue@gmea.net>
2018-08-16 19:51:21 +0200

    deporting play button into info panel

--- js/index.js
+++ js/index.js
@@ -42,4 +42,35 @@ $(document).ready(function () {
         $('.tapis-info').fadeOut(1000);
         $('.menu-zone').fadeIn(1000);
     });
+    
+    // insert play button
+    $('.play p').before('<svg viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg" width="50" height="50"><g><polygon points="0,0 100,50 0,100" fill="#009EF8" stroke-width="0"></polygon></g></svg>');
+    
+    // play
+    $('.play').on('click', function (e) {
+        e.preventDefault();
+        
+        // get player controls
+        var controls = $('#tapis-player').find('.controls');
+        
+        // if playing, stop the player
+        var status = $('#tapis-player').find('svg').attr('class');
+        status = status.replace('playable', '').trim();
+        console.log('status', status);
+        
+        // if playing, click on player to stop
+        if (status == 'playing')
+            controls.click();
+        
+        // get source to play
+        var src = $(this).attr('src');
+        console.log('src', src);
+        
+        // set player source
+        var audio = $('#tapis-player').find('audio');
+        audio.attr('src', src);
+        
+        // click on player to play
+        controls.click();
+    });
 });
--- js/player.js
+++ js/player.js
@@ -112,7 +112,7 @@
              * Default Options
              */
 
-            var template = ['<svg viewBox="0 0 100 100" id="playable" version="1.1" xmlns="http://www.w3.org/2000/svg" width="34" height="34" data-play="playable" class="not-started playable">',
+            var template = ['<svg viewBox="0 0 100 100" id="playable" version="1.1" xmlns="http://www.w3.org/2000/svg" width="100" height="100" data-play="playable" class="not-started playable">',
                 '<g class="shape">',
                 '<circle class="progress-track" cx="50" cy="50" r="47.45" stroke="#becce1" stroke-opacity="0.25" stroke-linecap="round" fill="none" stroke-width="5"/>',
                 '<circle class="precache-bar" cx="50" cy="50" r="47.45" stroke="#302F32" stroke-opacity="0.25" stroke-linecap="round" fill="none" stroke-width="5" transform="rotate(-90 50 50)"/>',
@@ -120,11 +120,11 @@
                 '</g>',
                 '<circle class="controls" cx="50" cy="50" r="45" stroke="none" fill="#000000" opacity="0.0" pointer-events="all"/>',
                 '<g class="control pause">',
-                '<line x1="40" y1="35" x2="40" y2="65" stroke="#000000" fill="none" stroke-width="8" stroke-linecap="round"/>',
-                '<line x1="60" y1="35" x2="60" y2="65" stroke="#000000" fill="none" stroke-width="8" stroke-linecap="round"/>',
+                '<line x1="40" y1="30" x2="40" y2="70" stroke="#becce1" stroke-opacity="0.25" fill="none" stroke-width="8" stroke-linecap="round"/>',
+                '<line x1="60" y1="30" x2="60" y2="70" stroke="#becce1" stroke-opacity="0.25" fill="none" stroke-width="8" stroke-linecap="round"/>',
                 '</g>',
                 '<g class="control play">',
-                '<polygon points="45,35 65,50 45,65" fill="#009EF8" stroke-width="0"></polygon>',
+                '<polygon points="30,25 80,50 30,75" fill="#009EF8" stroke-width="0"></polygon>',
                 '</g>',
                 '<g class="control stop">',
                 '<rect x="35" y="35" width="30" height="30" stroke="#000000" fill="none" stroke-width="1"/>',
@@ -137,8 +137,8 @@
                 
                 var audio = $(this).find('audio');
                 audio.attr('id', 'audio' + a);
-                template = template.replace('width="34"','width="'+ audio.data('size')  +'"');
-                template = template.replace('height="34"','height="'+ audio.data('size')  +'"');
+                template = template.replace('width="100"','width="'+ audio.data('size')  +'"');
+                template = template.replace('height="100"','height="'+ audio.data('size')  +'"');
                 template = template.replace('id="playable"', 'id="playable' + a + '"');
                 $(this).append(template);
                 
@@ -166,7 +166,7 @@
             $(_.settings.controlsObj).on('click', function (e) {
 
                 var el = $(e.currentTarget).closest($(_.settings.thisSelector));
-
+                
                 var obj = {
                     el         : el,
                     activeAudio: el.find(_.settings.audioObj),
